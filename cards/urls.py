f3b120ed21aca726788cdb78110d9204139e2e89
omar <omar96-alhamwi@hotmail.com>
2023-05-10 02:11:04 +0300

    Now we add About us section

--- cards/urls.py
+++ cards/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path("",views.home),
+    path("about/",views.About,name="about"),
+    path("contact/",views.Contact,name="contact"),
     path("category/<slug:val>",views.CategoryView.as_view(), name="category"),
-     path("category-title/<val>",views.categoryTitle.as_view(), name="category-title"),
+    path("category-title/<val>",views.categoryTitle.as_view(), name="category-title"),
     path("product-detail/<int:pk>",views.ProductDetail.as_view(), name="product-detail"),
     # path("new", views.CardCreateView.as_view(), name="card-create"),
 #     path("edit/<int:pk>", views.CardUpdateView.as_view(), name="card-update"),
